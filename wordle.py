715c7ffa0f8167f5b4c6b1d097b206e6353fbf64
master-puller <graviteewars@gmail.com>
2022-01-14 09:34:39 -0800

    Added comments  for readability
    Credit for code where it is due

--- wordle.py
+++ wordle.py
@@ -1,9 +1,9 @@
 #./bin/python3
-
+from typing import Tuple, List, Union
 from os.path import abspath, join
 from random import randint
 from collections import Counter
-try:
+try: # Module not installed for everyone
     from termcolor import colored
     COLOR = True
 except:
@@ -22,16 +22,17 @@ def _count_gen(reader):
         yield b
         b = reader(1 << 20)
 
-def random_line(file):
-    line = next(file)
-    for index, _line in enumerate(file, 2):
-        if not randint(0, index):
+def random_line(stream) -> str:
+    # https://stackoverflow.com/questions/3540288/how-do-i-read-a-random-line-from-one-file#3540315
+    line = next(stream)
+    for index, _line in enumerate(stream, 2):
+        if not randint(0, stream):
             line = _line
     return str(line)
 
 class Wordle_Game:
-
-    def __init__(self, wordspath):
+    '''Game class implementing wordle, no word validation'''
+    def __init__(self, wordspath) -> None:
         with open(wordspath, 'rb') as wordslist:
             self._word = random_line(wordslist)[2:-2].upper()
         self.length = len(self._word)
@@ -39,36 +40,45 @@ class Wordle_Game:
         self._counter = Counter(self._word)
         self.won = False
 
-    def __len__(self):
+    def __len__(self) -> int:
+        '''returns how many attempts the player has made'''
+        # len(Game) --> attempts
         return self.attempts
 
     def _validate(self, word):
-        """Validation code, returns bulls (correct) and cows (wrong place)"""
-        bull, cow = 0, self._counter & Counter(word)
-        res = [None] * len(word)
-        cows = [" "] * len(word)
+        ''' Do not use. Internal function'''
+        bull, cow = 0, self._counter & Counter(word) # Counter intersection, letters match but wrong place
+        res = [None] * len(word) # Preallocate to save memory and increase speed
+        cows = [None] * len(word)
+        # First pass matches the letters together
         for idx, (x,y) in enumerate(zip(self._word, word)):
             if x==y: # Bull
                 cow[x] -= 1
                 bull += 1
                 res[idx] = colored(x, 'green', attrs=['reverse'])
 
+        # Second pass makes sure that wrong place letters processed
         for idx, x in enumerate(word):
             if res[idx] == None:
                 if cow[x] > 0:
-                    cow[x] -= 1
+                    cow[x] -= 1 # Subtract one to not have duplicates
                     if COLOR:
                         res[idx] = colored(x, 'yellow')
-                        cows[idx] = "*"
                     else:
-                        res[idx] = "*"
                         cows[idx] = x
                 else:
                     res[idx] = '*'
-        return ''.join(res), ''.join(cows), bull, len(cow)
+        return res, cows, bull, len(cow)
 
-    def guess(self, word):
+    def guess(self, word:str) -> Tuple[List[str], List[str], Int, Int]:
+        '''Outside interface for guesses:
+        Returns:
+            bulls: Array of characters with correct letters in place
+            cows : Array of characters with letters in incorrect places
+            bull : count of correct characters
+            cow  : count of characters in wrong place'''
         if self.won:
+            # Already won, no point
             return
         word, cows, bull, cow = self._validate(word)
         self.attempts += 1
@@ -76,25 +86,32 @@ class Wordle_Game:
             self.won = True
         return word, cows, bull, cow
 
-def wordle(words="passwords.txt"):
+def wordle_player(words="passwords.txt") -> bool:
+    '''Simple human interface for game'''
     wordspath = join(ABSPATH, words)
     try:
         while True:
+            # Main game loop
             game = Wordle_Game(wordspath)
+            # Guess length
             print("WORD:\n[", "*" * game.length, "]") 
             while not game.won:
                 inp = input("> ").upper()
                 if len(inp) != game.length:
-                    print("  ERR: WRONG LENGTH")
+                    print("  ERR: WRONG LENGTH") # Undefined case if shorter than word
                     continue
-                word, cow, b,c = game.guess(inp)
-                print(f"\r> {word} : {len(game)}")
+                word, cow, bullcount, cowcount = game.guess(inp)
+                print(f"\r> {''.join(word)} : {len(game)}")
                 if not COLOR and c != 0:
-                    print(f"> {cow} Misplaced")
+                    # Gives generic hints as to letter composition
+                    print(f"> {''.join(cow)} Misplaced")
             else:
                 print("  Victory! ")
-    except EOFError:
-        print(f"> Word was:\n> {game._word}") 
+                return True
+    except EOFError or KeyboardInterrupt:
+        # Exit conditions, otherwise throw an exception
+        print(f"> Word was:\n> {game._word}")
+    return False
 
 if __name__ == "__main__":
-    wordle()
+    wordle_player()
